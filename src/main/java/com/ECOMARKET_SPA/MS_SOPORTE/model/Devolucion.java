fafea0bdb928d5c7105a1dde020f91c542d77de4
Felipe Inzunza <feli.inzunza@duocuc.cl>
2025-05-16 02:41:44 -0400

    cambioendevolucion

--- src/main/java/com/ECOMARKET_SPA/MS_SOPORTE/model/Devolucion.java
+++ src/main/java/com/ECOMARKET_SPA/MS_SOPORTE/model/Devolucion.java
@@ -1,37 +1,24 @@
 package com.ECOMARKET_SPA.MS_SOPORTE.model;
 
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Table(name = "devolucion")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
+@Data
+@AllArgsConstructor
+@Table(name = "devolucion")
 public class Devolucion {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idDevolucion;
 
-    /*@ManyToOne
-    @JoinColumn(name = "pedido_id", nullable = false)*/
-    private int idPedido; //Por mientras solo guardamos el id del pedido, no la entidad completa
+    /*@Column(nullable = false)
+    private List<ProductoDevolucion> productosDevolucion;*/
 
-    @OneToOne(mappedBy = "devolucion")
-    @JsonBackReference
-    private Ticket ticket;
-
-    // Getters y Setters
-
-}
+} 
