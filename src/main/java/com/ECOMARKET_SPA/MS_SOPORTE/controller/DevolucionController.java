3895fd829faa9b52aa952b369f10c2ee0d9b5b03
Felipe Inzunza <feli.inzunza@duocuc.cl>
2025-05-16 11:19:50 -0400

    c

--- src/main/java/com/ECOMARKET_SPA/MS_SOPORTE/controller/DevolucionController.java
+++ src/main/java/com/ECOMARKET_SPA/MS_SOPORTE/controller/DevolucionController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/devoluciones")
+@RequestMapping("/api/devoluciones")
 public class DevolucionController {
     
     @Autowired
--- src/main/java/com/ECOMARKET_SPA/MS_SOPORTE/service/DevolucionService.java
+++ src/main/java/com/ECOMARKET_SPA/MS_SOPORTE/service/DevolucionService.java
@@ -28,5 +28,6 @@ public class DevolucionService {
 
     public void eliminarDevolucion(int idDevolucion) {
         devolucionRepository.deleteById(idDevolucion);
+        
     }
 }
